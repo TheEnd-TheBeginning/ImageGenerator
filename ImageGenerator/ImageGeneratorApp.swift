653ea876b4418d599a2e67a522b9597f271350b0
Al <asavicheva@internet.ru>
2026-03-31 15:36:58 +0300

    Initial Commit

---
+++ ImageGenerator/ImageGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGeneratorApp.swift
+//  ImageGenerator
+//
+//  Created by Александра Савичева on 31.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct ImageGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
